3d3e3b4c57d903df99a17af217de637eb66e33ed
jgu7man <jgu7man@gmail.com>
2021-05-28 18:53:25 -0500

    change in libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { GdevMainService } from './gdev-panel/gdev-main.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bionumedics';
+  constructor(
+    @Inject(DOCUMENT) private doc,
+    private _admin: GdevMainService,
+  ) {
+    this._admin.getBrandInfo().subscribe(info => {
+      if (info && info.iconLogo) {
+        let link: HTMLLinkElement = this.doc.createElement('link');
+        link.setAttribute('rel', 'icon');
+        link.setAttribute('type', 'image/x-icon');
+        link.setAttribute('href', info.iconLogo.url);
+        this.doc.head.appendChild(link);
+      }
+    })
+   }
+
+
 }
